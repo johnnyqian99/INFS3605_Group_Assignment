3c43edc851d3edffa18ffeb4debf24a048b88fa4
Kevin Surjadi <kevinsurjadi@outlook.com>
2021-07-03 15:26:11 +1000

    Login Screen

---
+++ app/src/main/java/com/example/infs3605_group_assignment/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.infs3605_group_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        // splash screen length
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(mLaunchApp, 300);
+    }
+
+    private Runnable mLaunchApp = new Runnable() {
+        public void run() {
+            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(i);
+        }
+    };
+}
