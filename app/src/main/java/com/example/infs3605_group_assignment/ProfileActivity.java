51d5635a0de126fe31f28796aa5161ff477243ba
johnnyqian99 <71432625+johnnyqian99@users.noreply.github.com>
2021-07-21 17:17:02 +1000

    Prep Navigation Bar

---
+++ app/src/main/java/com/example/infs3605_group_assignment/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.infs3605_group_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
